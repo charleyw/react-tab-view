c50245f3a7f951f3687f6895990fd04805811df4
Chao Wang <cwang8023@gmail.com>
2016-01-01 23:31:44 +0800

    make original one work

--- example/index.jsx
+++ example/index.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
+import ReactTabView from '../src/react-tab-view'
+
+let App = React.createClass({
+  render:function(){
+    return (<div className="container">
+      <ReactTabView baseWidth={400}>
+        <div name="red">
+          <div className="red">我是红色的标签页</div>
+        </div>
+        <div name="blue">
+          <div className="blue">我是蓝色的标签页</div>
+        </div>
+        <div name="yellow">
+          <div className="yellow">我是黄色的标签页</div>
+        </div>
+      </ReactTabView>
+    </div>);
+  }
+});
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App/>,
   document.getElementById('app')
 );
--- src/react-tab-view.jsx
+++ src/react-tab-view.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import './react-tab-view.css';
 
-
 export default React.createClass({
-  render: function() {
-    return <div className="react-tab-view">Hello World</div>;
+  getInitialState: function(){
+    return {currentIndex: 0}
+  },
+  getTitleItemCssClasses: function(index){
+    return index === this.state.currentIndex ? "tab-title-item active" : "tab-title-item";
+  },
+
+  getContentItemCssClasses: function(index){
+    return index === this.state.currentIndex ? "tab-content-item active" : "tab-content-item";
+  },
+
+  render: function(){
+    let that = this;
+    let {baseWidth} = this.props;
+    let childrenLength = this.props.children.length;
+    return (
+      <div className="react-tab-view">
+        <nav className="tab-title-items">
+          {React.Children.map(this.props.children, (element, index) => {
+            return (<div onClick={() => {this.setState({currentIndex: index})}} className={that.getTitleItemCssClasses(index)}>{element.props.name}</div>)
+          })}
+        </nav>
+        <div className="tab-content-items">
+          {React.Children.map(this.props.children, (element, index) => {
+            return (<div className={that.getContentItemCssClasses(index)}>{element}</div>)
+          })}
+        </div>
+      </div>
+    )
   }
 });
